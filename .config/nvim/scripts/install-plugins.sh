69301af95da26be0f5dbdc0d0987aa575df69d49
Olaf Luijks <olafluijks@proton.me>
2025-12-05 09:07:15 +0100

    fix: check if plugin is already installed

--- .config/nvim/scripts/install-plugins.sh
+++ .config/nvim/scripts/install-plugins.sh
@@ -5,114 +5,138 @@ set -euo pipefail
 NVIM_DATA="${XDG_DATA_HOME:-$HOME/.local/share}/nvim"
 
 echo "Using NVIM_DATA=$NVIM_DATA"
+echo
+
+clone_if_missing() {
+  local name="$1"
+  local url="$2"
+  local dest="$3"
+
+  if [ -d "$dest/.git" ]; then
+    echo "[SKIP] $name already installed at $dest"
+    return
+  fi
+
+  if [ -e "$dest" ] && [ ! -d "$dest/.git" ]; then
+    echo "[WARN] $name destination exists but is not a git repo: $dest"
+    echo "       Skipping; please inspect/clean this path manually if needed."
+    return
+  fi
+
+  echo "[CLONE] $name → $dest"
+  mkdir -p "$(dirname "$dest")"
+  git clone --depth=1 "$url" "$dest"
+  echo
+}
 
 # ------------------------------------------------------------------------------
 # Formatting
 # ------------------------------------------------------------------------------
 
-# stevearc/conform.nvim
-mkdir -p "$NVIM_DATA/site/pack/conform/start"
-git clone --depth=1 https://github.com/stevearc/conform.nvim \
+clone_if_missing \
+  "conform.nvim" \
+  "https://github.com/stevearc/conform.nvim" \
   "$NVIM_DATA/site/pack/conform/start/conform.nvim"
 
 # ------------------------------------------------------------------------------
 # UI, statusline, keymaps, commandline
 # ------------------------------------------------------------------------------
 
-# nvim-tree/nvim-web-devicons
-mkdir -p "$NVIM_DATA/site/pack/nvim-web-devicons/start"
-git clone --depth=1 https://github.com/nvim-tree/nvim-web-devicons.git \
+clone_if_missing \
+  "nvim-web-devicons" \
+  "https://github.com/nvim-tree/nvim-web-devicons.git" \
   "$NVIM_DATA/site/pack/nvim-web-devicons/start/nvim-web-devicons"
 
-# nvim-lualine/lualine.nvim
-mkdir -p "$NVIM_DATA/site/pack/lualine/start"
-git clone --depth=1 https://github.com/nvim-lualine/lualine.nvim.git \
+clone_if_missing \
+  "lualine.nvim" \
+  "https://github.com/nvim-lualine/lualine.nvim.git" \
   "$NVIM_DATA/site/pack/lualine/start/lualine.nvim"
 
-# folke/which-key.nvim
-mkdir -p "$NVIM_DATA/site/pack/which-key/start"
-git clone --depth=1 https://github.com/folke/which-key.nvim.git \
+clone_if_missing \
+  "which-key.nvim" \
+  "https://github.com/folke/which-key.nvim.git" \
   "$NVIM_DATA/site/pack/which-key/start/which-key.nvim"
 
-# MunifTanjim/nui.nvim (UI library for noice)
-mkdir -p "$NVIM_DATA/site/pack/nui/start"
-git clone --depth=1 https://github.com/MunifTanjim/nui.nvim.git \
+clone_if_missing \
+  "nui.nvim" \
+  "https://github.com/MunifTanjim/nui.nvim.git" \
   "$NVIM_DATA/site/pack/nui/start/nui.nvim"
 
-# folke/noice.nvim (commandline/messages UI)
-mkdir -p "$NVIM_DATA/site/pack/noice/start"
-git clone --depth=1 https://github.com/folke/noice.nvim.git \
+clone_if_missing \
+  "noice.nvim" \
+  "https://github.com/folke/noice.nvim.git" \
   "$NVIM_DATA/site/pack/noice/start/noice.nvim"
 
 # ------------------------------------------------------------------------------
 # Colorschemes & visuals
 # ------------------------------------------------------------------------------
 
-# folke/tokyonight.nvim
-mkdir -p "$NVIM_DATA/site/pack/tokyonight/start"
-git clone --depth=1 https://github.com/folke/tokyonight.nvim.git \
+clone_if_missing \
+  "tokyonight.nvim" \
+  "https://github.com/folke/tokyonight.nvim.git" \
   "$NVIM_DATA/site/pack/tokyonight/start/tokyonight.nvim"
 
-# ellisonleao/gruvbox.nvim
-mkdir -p "$NVIM_DATA/site/pack/gruvbox/start"
-git clone --depth=1 https://github.com/ellisonleao/gruvbox.nvim.git \
+clone_if_missing \
+  "gruvbox.nvim" \
+  "https://github.com/ellisonleao/gruvbox.nvim.git" \
   "$NVIM_DATA/site/pack/gruvbox/start/gruvbox.nvim"
 
-# brenoprata10/nvim-highlight-colors
-mkdir -p "$NVIM_DATA/site/pack/nvim-highlight-colors/start"
-git clone --depth=1 https://github.com/brenoprata10/nvim-highlight-colors.git \
+clone_if_missing \
+  "nvim-highlight-colors" \
+  "https://github.com/brenoprata10/nvim-highlight-colors.git" \
   "$NVIM_DATA/site/pack/nvim-highlight-colors/start/nvim-highlight-colors"
 
 # ------------------------------------------------------------------------------
 # Core libs, treesitter, pickers, explorer, snacks
 # ------------------------------------------------------------------------------
 
-# nvim-lua/plenary.nvim
-mkdir -p "$NVIM_DATA/site/pack/plenary/start"
-git clone --depth=1 https://github.com/nvim-lua/plenary.nvim.git \
+clone_if_missing \
+  "plenary.nvim" \
+  "https://github.com/nvim-lua/plenary.nvim.git" \
   "$NVIM_DATA/site/pack/plenary/start/plenary.nvim"
 
-# nvim-telescope/telescope.nvim
-mkdir -p "$NVIM_DATA/site/pack/telescope/start"
-git clone --depth=1 https://github.com/nvim-telescope/telescope.nvim.git \
+clone_if_missing \
+  "telescope.nvim" \
+  "https://github.com/nvim-telescope/telescope.nvim.git" \
   "$NVIM_DATA/site/pack/telescope/start/telescope.nvim"
 
-# nvim-treesitter/nvim-treesitter
-mkdir -p "$NVIM_DATA/site/pack/nvim-treesitter/start"
-git clone --depth=1 https://github.com/nvim-treesitter/nvim-treesitter.git \
+clone_if_missing \
+  "nvim-treesitter" \
+  "https://github.com/nvim-treesitter/nvim-treesitter.git" \
   "$NVIM_DATA/site/pack/nvim-treesitter/start/nvim-treesitter"
 
-# nvim-treesitter/nvim-treesitter-textobjects
-mkdir -p "$NVIM_DATA/site/pack/nvim-treesitter-textobjects/start"
-git clone --depth=1 https://github.com/nvim-treesitter/nvim-treesitter-textobjects.git \
+clone_if_missing \
+  "nvim-treesitter-textobjects" \
+  "https://github.com/nvim-treesitter/nvim-treesitter-textobjects.git" \
   "$NVIM_DATA/site/pack/nvim-treesitter-textobjects/start/nvim-treesitter-textobjects"
 
-# folke/snacks.nvim (Explorer, Picker, Lazygit, Input, etc.)
-mkdir -p "$NVIM_DATA/site/pack/snacks/start"
-git clone --depth=1 https://github.com/folke/snacks.nvim.git \
+clone_if_missing \
+  "snacks.nvim" \
+  "https://github.com/folke/snacks.nvim.git" \
   "$NVIM_DATA/site/pack/snacks/start/snacks.nvim"
 
 # ------------------------------------------------------------------------------
 # Git integration
 # ------------------------------------------------------------------------------
 
-# lewis6991/gitsigns.nvim
-mkdir -p "$NVIM_DATA/site/pack/gitsigns/start"
-git clone --depth=1 https://github.com/lewis6991/gitsigns.nvim.git \
+clone_if_missing \
+  "gitsigns.nvim" \
+  "https://github.com/lewis6991/gitsigns.nvim.git" \
   "$NVIM_DATA/site/pack/gitsigns/start/gitsigns.nvim"
 
-# OPTIONAL: kdheepak/lazygit.nvim (original wrapper; Snacks.lazygit() is usually enough)
-mkdir -p "$NVIM_DATA/site/pack/lazygit/start"
-git clone --depth=1 https://github.com/kdheepak/lazygit.nvim.git \
+# OPTIONAL: kdheepak/lazygit.nvim (primary entry is Snacks.lazygit())
+clone_if_missing \
+  "lazygit.nvim (optional)" \
+  "https://github.com/kdheepak/lazygit.nvim.git" \
   "$NVIM_DATA/site/pack/lazygit/start/lazygit.nvim"
 
 # ------------------------------------------------------------------------------
 # Tmux integration
 # ------------------------------------------------------------------------------
 
-# christoomey/vim-tmux-navigator
-mkdir -p "$NVIM_DATA/site/pack/tmux-navigator/start"
-git clone --depth=1 https://github.com/christoomey/vim-tmux-navigator.git \
+clone_if_missing \
+  "vim-tmux-navigator" \
+  "https://github.com/christoomey/vim-tmux-navigator.git" \
   "$NVIM_DATA/site/pack/tmux-navigator/start/vim-tmux-navigator"
 
-echo "Done. Native plugins installed under $NVIM_DATA/site/pack"
+echo "Done. Native plugins installed/checked under $NVIM_DATA/site/pack"
